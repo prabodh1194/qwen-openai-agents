bfb147c8b5f63025633ab19eabde4a1553ac00f9
Prabodh Agarwal <prabodh1194@users.noreply.github.com>
2025-09-18 06:51:54 +0530

    fix the lambda

--- deployment/scripts/build.py
+++ deployment/scripts/build.py
@@ -63,7 +63,6 @@ def build_lambda_package() -> Path:
         lambda_handlers = [
             "lambda_handler.py",
             "refresh_creds_handler.py",
-            "scheduled_invoke_handler.py",
         ]
         for handler in lambda_handlers:
             handler_path = lambda_dir / handler
